0e52922ca6844af97031adbfe954a2f1ad082d30
taind <dinhtai2000@gmail.com>
2023-06-27 21:10:52 +0700

    Buoi 1

---
+++ Assets/Scripts/Nhay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nhay : MonoBehaviour
+{
+    public Rigidbody2D myBody;
+    public float jumpForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+            if (Bấm phím cách)
+            thì 
+            {
+                con chim sẽ bay lên
+            }
+         */
+        bool isTap = Input.GetKeyDown(KeyCode.Space);
+        if (isTap)
+        {
+            Debug.Log("Đã bấm phím cách");
+            myBody.velocity = new Vector2(0, jumpForce);
+            // old: 0, 0 + (0, 10) => (0,10)
+        }
+    }
+}
